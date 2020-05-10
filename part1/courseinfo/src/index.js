93d08a9e5b48fc6e98647d5a9110081547bdf6d3
Danish Parvez <22967284+sweatwork@users.noreply.github.com>
2020-05-10 13:26:55 +0530

    Refactor module

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,98 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const Header = ({name}) => (
-    <h2>
-      {name}
-    </h2>
-  )
-
-const Part = ({name, exercises}) => (
-    <p>
-      {name} {exercises}
-    </p>
-  )
-
-const Content = ({parts}) => parts.map(part => (
-  <Part key={part.id} name={part.name} exercises={part.exercises} /> 
-  ))
-
-const Course = ({course}) => {
-  return (
-    <div key={course.id}>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-    </div>
-  ) 
-}
-
-const Total = ({parts}) => {
-    const total = parts.reduce( (s,p) => s + p.exercises, 0 )
-
-    return (
-      <b>
-        total of {total} exercises
-      </b>
-    )
-  }
-
-const App = () => {
-  const courses = [
+const courses = [
   {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     id: 1,
     parts: [
       {
-        name: 'Fundamentals of React',
+        name: "Fundamentals of React",
         exercises: 10,
-        id: 1
+        id: 1,
       },
       {
-        name: 'Using props to pass data',
+        name: "Using props to pass data",
         exercises: 7,
-        id: 2
+        id: 2,
       },
       {
-        name: 'State of a component',
+        name: "State of a component",
         exercises: 14,
-        id: 3
+        id: 3,
       },
       {
-        name: 'Redux',
+        name: "Redux",
         exercises: 11,
-        id: 4
-      }
-    ]
+        id: 4,
+      },
+    ],
   },
   {
-    name: 'Node.js',
+    name: "Node.js",
     id: 2,
     parts: [
       {
-        name: 'Routing',
+        name: "Routing",
         exercises: 3,
-        id: 1
+        id: 1,
       },
       {
-        name: 'Middlewares',
+        name: "Middlewares",
         exercises: 7,
-        id: 2
-      }
-    ]
-  }
-]
+        id: 2,
+      },
+    ],
+  },
+];
 
-  return (
-    // returns as a fragment to keep the DOM clean 
-    <>
-    <h1>Web development curriculum</h1>
-    {courses.map(course => <Course key={course.id} course={course} /> )}
-    </ >
-  )
-}
-
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App courses={courses} />, document.getElementById("root"));

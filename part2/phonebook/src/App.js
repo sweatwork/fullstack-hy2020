a31a47579a3758cac7a639da3fcbe0d3f4b95033
Danish Parvez <22967284+sweatwork@users.noreply.github.com>
2020-05-13 13:02:23 +0530

    Prevents adding duplicate names to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,40 +1,50 @@
-import React, {useState} from 'react';
-import Name from './components/Name'
+import React, { useState } from "react";
+import Name from "./components/Name";
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState("")
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
 
   const addPerson = (event) => {
-    event.preventDefault()
-    
+    event.preventDefault();
+
     const personObject = {
       name: newName,
-      id: newName
+      id: newName,
+    };
+
+    const nameCopy = persons.find((person) => person.name === newName);
+
+    if (nameCopy === undefined) {
+      setPersons(persons.concat(personObject));
+    } else {
+      alert(`${newName} is already added to phonebook`);
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-  }
-  
+
+    setNewName("");
+  };
+
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <Name key={person.id} person={person.name} /> )}
+      {persons.map((person) => (
+        <Name key={person.id} person={person.name} />
+      ))}
     </div>
-  )
+  );
+};
 
-}
 export default App;

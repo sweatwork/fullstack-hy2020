a609551b417fcbc023522403014168e4d7a71c1f
Danish Parvez <22967284+sweatwork@users.noreply.github.com>
2020-05-15 15:49:55 +0530

    Refactor component

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-const Persons = ({ namesToShow }) => {
-  return namesToShow.map((person) => (
-    <div>
+const Persons = ({ personsToShow }) => {
+  return personsToShow.map((person) => (
+    <div key={person.id}>
       {person.name} {person.number}
     </div>
   ));
 };
-
 export default Persons;

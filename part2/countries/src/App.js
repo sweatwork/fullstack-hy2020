074e49044501093046ae1d3c4d3e0c28db296c92
Danish Parvez <22967284+sweatwork@users.noreply.github.com>
2020-05-15 22:21:18 +0530

    Implement user interface

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = (props) => {
+    const [country, setCountry] = useState("") 
+    
+    const handleCountry = (event) => {
+        setCountry(event.target.value)
+    }
+    
+    return (
+        <div>
+            find countries<input value={country} onChange={handleCountry}/>
+        </div>    
+    )
 }
 
-export default App;
+export default App

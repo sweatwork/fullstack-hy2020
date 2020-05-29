9d26d4a3c303ca1b3fb0657e1926eec8dfe4c7e9
Danish Parvez <22967284+sweatwork@users.noreply.github.com>
2020-05-29 18:29:03 +0530

    Add proxy

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "https://fathomless-tundra-81713.herokuapp.com/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
